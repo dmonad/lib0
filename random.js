30710aff317f268767b24580fa86a3f64cb12623
Kevin Jahns <kevin.jahns@protonmail.com>
2019-12-05 15:35:17 +0100

    implement guid generator

--- random.js
+++ random.js
@@ -7,6 +7,8 @@ import * as env from './environment.js'
 import * as binary from './binary.js'
 import * as math from './math.js'
 
+const nodeCrypto = env.isNode ? require('crypto') : null
+
 /**
  * Basically Math.random. Returns a pseudo random number in [0,1).
  */
@@ -22,20 +24,12 @@ const uint32BrowserCrypto = () => {
 /* istanbul ignore next */
 const uint32NoCrypto = () => math.ceil((rand() * binary.BITS32) >>> 0)
 
-/**
- * @param {typeof import('crypto')} crypto
- * @return {function():number}
- */
-const uint32NodeCrypto = crypto => () => {
-  // @ts-ignore
-  const buf = crypto.randomBytes(4)
-  return new Uint32Array(buf.buffer)[0]
-}
+const uint32NodeCrypto = () => new Uint32Array(/** @type {any} */ (nodeCrypto).randomBytes(4).buffer)[0]
 
 /* istanbul ignore next */
-export const uint32 = env.isBrowser
-  ? (typeof crypto === 'undefined' ? uint32NoCrypto : uint32BrowserCrypto)
-  : uint32NodeCrypto(require('crypto'))
+export const uint32 = env.isNode
+  ? uint32NodeCrypto
+  : (typeof crypto === 'undefined' ? uint32NoCrypto : uint32BrowserCrypto)
 
 /**
  * @template T
@@ -43,3 +37,9 @@ export const uint32 = env.isBrowser
  * @return {T}
  */
 export const oneOf = arr => arr[math.floor(rand() * arr.length)]
+
+// @ts-ignore
+const uuidv4Template = [1e7] + -1e3 + -4e3 + -8e3 + -1e11
+export const uuidv4 = () => uuidv4Template.replace(/[018]/g, /** @param {number} c */ c =>
+  (c ^ uint32() & 15 >> c / 4).toString(16)
+)
--- random.test.js
+++ random.test.js
@@ -1,6 +1,7 @@
 import * as random from './random.js'
 import * as t from './testing.js'
 import * as binary from './binary.js'
+import * as math from './math.js'
 
 /**
  * @param {t.TestCase} tc
@@ -16,4 +17,32 @@ export const testUint32 = tc => {
   }
   t.info(`Largest number generated is ${num} (0x${num.toString(16)})`)
   t.assert((num & binary.BIT32) !== 0, 'Largest number is 32 bits long.')
+}
+
+/**
+ * @param {t.TestCase} tc
+ */
+export const testUuidv4 = tc => {
+  t.info(`Generated a UUIDv4: ${random.uuidv4()}`)
+}
+
+/**
+ * @param {t.TestCase} tc
+ */
+export const testUuidv4Overlaps = tc => {
+  t.skip(!t.production)
+  const iterations = 1000000
+  const uuids = new Set()
+  for (let i = 0; i < iterations; i++) {
+    const uuid = random.uuidv4()
+    if (uuids.has(uuid)) {
+      t.fail('uuid already exists')
+    } else {
+      uuids.add(uuid)
+    }
+    if (uuids.size % (iterations / 20) === 0) {
+      t.info(`${math.round(uuids.size * 100 / iterations)}% complete`)
+    }
+  }
+  t.assert(uuids.size === iterations)
 }
