ccea08231c4d8bdb56e97750291505d1f1a41046
Kevin Jahns <kevin.jahns@pm.me>
2020-01-13 07:43:27 +0100

    make isormorphic exports any types

--- isomorphic.js
+++ isomorphic.js
@@ -1,5 +1,5 @@
 // @ts-ignore
 import iso from 'isomorphic.js'
 
-export const performance = iso.performance
-export const cryptoRandomBuffer = iso.cryptoRandomBuffer
+export const performance = /** @type {any} */ (iso.performance)
+export const cryptoRandomBuffer = /** @type {any} */ (iso.cryptoRandomBuffer)
