70e2d14bb0e3f3faaf6c0845e76adc89b0cc419b
Kevin Jahns <kevin.jahns@pm.me>
2020-03-02 14:19:25 +0100

    code coverage human time format

--- time.js
+++ time.js
@@ -20,10 +20,11 @@ export const humanizeDuration = d => {
   const hours = math.floor(d / 3600) % 24
   const days = math.floor(d / 86400)
   if (days > 0) {
-    return days + 'd' + (hours > 0 ? ' ' + (minutes > 30 ? hours + 1 : hours) + 'h' : '')
+    return days + 'd' + ((hours > 0 || minutes > 30) ? ' ' + (minutes > 30 ? hours + 1 : hours) + 'h' : '')
   }
   if (hours > 0) {
-    return hours + 'h' + (minutes > 0 ? ' ' + (seconds > 30 ? minutes + 1 : minutes) + 'min' : '')
+    /* istanbul ignore next */
+    return hours + 'h' + ((minutes > 0 || seconds > 30) ? ' ' + (seconds > 30 ? minutes + 1 : minutes) + 'min' : '')
   }
   return minutes + 'min' + (seconds > 0 ? ' ' + seconds + 's' : '')
 }
