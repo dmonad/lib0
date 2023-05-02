4c259c57abe273b68fd2d5e0744be95d66674e2b
Kevin Jahns <kevin.jahns@protonmail.com>
2023-05-02 20:25:43 +0200

    [number] add HIGHEST_UINT32

--- number.js
+++ number.js
@@ -11,14 +11,8 @@ export const MAX_SAFE_INTEGER = Number.MAX_SAFE_INTEGER
 export const MIN_SAFE_INTEGER = Number.MIN_SAFE_INTEGER
 
 export const LOWEST_INT32 = 1 << 31
-/**
- * @type {number}
- */
 export const HIGHEST_INT32 = binary.BITS31
-
-/**
- * @module number
- */
+export const HIGHEST_UINT32 = binary.BITS32
 
 /* c8 ignore next */
 export const isInteger = Number.isInteger || (num => typeof num === 'number' && isFinite(num) && math.floor(num) === num)
