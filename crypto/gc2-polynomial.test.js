96bbda0b958cdc6f2da427006b75f66bc5b09170
Kevin Jahns <kevin.jahns@protonmail.com>
2023-06-16 19:18:13 +0200

    prelim work on rabin fingerprint

---
+++ crypto/gc2-polynomial.test.js
@@ -0,0 +1,106 @@
+import * as t from '../testing.js'
+import * as gc2 from './gc2-polynomial.js'
+import * as math from '../math.js'
+import * as array from '../array.js'
+import * as prng from '../prng.js'
+import * as buffer from '../buffer.js'
+
+/**
+ * @param {t.TestCase} _tc
+ */
+export const testPolynomialBasics = _tc => {
+  const bs = new Uint8Array([1, 11])
+  const p = gc2.createFromBytes(bs)
+  t.assert(p.degrees.has(3))
+  t.assert(p.degrees.has(1))
+  t.assert(p.degrees.has(0))
+  t.assert(p.degrees.has(8))
+}
+
+/**
+ * @param {t.TestCase} _tc
+ */
+export const testIrreducibleInput = _tc => {
+  const pa = gc2.createFromUint(0x53)
+  const pb = gc2.createFromUint(0xCA)
+  const pm = gc2.createFromUint(0x11B)
+  const px = gc2.multiply(pa, pb)
+  t.compare(new Uint8Array([0x53]), gc2.toUint8Array(pa))
+  t.compare(new Uint8Array([0xCA]), gc2.toUint8Array(pb))
+  t.assert(gc2.equals(gc2.createFromUint(0x3F7E), px))
+  t.compare(new Uint8Array([0x3F, 0x7E]), gc2.toUint8Array(px))
+  const pabm = gc2.mod(px, pm)
+  t.compare(new Uint8Array([0x1]), gc2.toUint8Array(pabm))
+}
+
+/**
+ * @param {t.TestCase} _tc
+ */
+export const testIrreducibleSpread = _tc => {
+  const degree = 53
+  const N = 200
+  const avgSpread = getSpreadAverage(degree, N)
+  const diffSpread = math.abs(avgSpread - degree)
+  t.info(`Average spread for degree ${degree} at ${N} repetitions: ${avgSpread}`)
+  t.assert(diffSpread < 3, 'Spread of irreducible polynomials is within expected range')
+}
+
+/**
+ * @param {number} degree
+ * @param {number} tests
+ */
+const getSpreadAverage = (degree, tests) => {
+  const spreads = []
+  for (let i = 0, test = 0, lastI = 0; test < tests; i++) {
+    const f = gc2.createRandom(degree)
+    t.assert(gc2.getHighestDegree(f) === degree)
+    if (gc2.isIrreducibleBenOr(f)) {
+      const spread = i - lastI
+      spreads.push(spread)
+      lastI = i
+      test++
+    }
+  }
+  return array.fold(spreads, 0, math.add) / tests
+}
+
+/**
+ * @param {t.TestCase} tc
+ */
+export const testFingerprint = tc => {
+  /**
+   * @type {Array<Uint8Array>}
+   */
+  const dataObjects = []
+  const N = 3000
+  const K = 53
+  const MSIZE = 130
+  const irreducible = gc2.createIrreducible(K)
+  t.info(`N=${N} K=${K} MSIZE=${MSIZE}`)
+  for (let i = 0; i < N; i++) {
+    dataObjects.push(prng.uint8Array(tc.prng, MSIZE))
+  }
+  /**
+   * @type {Array<Uint8Array>}
+   */
+  let fingerprints1 = []
+  t.measureTime('polynomial direct', () => {
+    fingerprints1 = dataObjects.map((o, _index) => gc2.fingerprint(o, irreducible))
+  })
+  const testSet = new Set(fingerprints1.map(buffer.toBase64))
+  t.assert(testSet.size === N)
+  /**
+   * @type {Array<Uint8Array>}
+   */
+  let fingerprints2 = []
+  t.measureTime('polynomial incremental', () => {
+    fingerprints2 = dataObjects.map((o, _index) => {
+      const encoder = new gc2.FingerprintEncoder(irreducible)
+      for (let i = 0; i < o.byteLength; i++) {
+        encoder.write(o[i])
+      }
+      return encoder.getFingerprint()
+    })
+  })
+  t.compare(fingerprints1, fingerprints2)
+}
--- number.js
+++ number.js
@@ -18,3 +18,20 @@ export const HIGHEST_UINT32 = binary.BITS32
 export const isInteger = Number.isInteger || (num => typeof num === 'number' && isFinite(num) && math.floor(num) === num)
 export const isNaN = Number.isNaN
 export const parseInt = Number.parseInt
+
+/**
+ * Count the number of "1" bits in an unsigned 32bit number.
+ *
+ * Super fun bitcount algorithm by Brian Kernighan.
+ *
+ * @param {number} n
+ */
+export const countBits = n => {
+  n &= binary.BITS32
+  let count = 0
+  while (n) {
+    n &= (n - 1)
+    count++
+  }
+  return count
+}
