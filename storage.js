c7382e8f0803a941e70cd0fcd6ca8c3d79f7b2b6
Kevin Jahns <kevin.jahns@protonmail.com>
2023-10-13 12:44:42 +0200

    [storage] more generic localStorage check #80

--- storage.js
+++ storage.js
@@ -40,7 +40,7 @@ let usePolyfill = true
 /* c8 ignore start */
 try {
   // if the same-origin rule is violated, accessing localStorage might thrown an error
-  if (typeof localStorage !== 'undefined' && localStorage !== null) {
+  if (typeof localStorage !== 'undefined' && localStorage) {
     _localStorage = localStorage
     usePolyfill = false
   }
