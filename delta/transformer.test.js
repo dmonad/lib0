4467e3fc2ad5616bb69af8d98168b3d95812c9d0
Kevin Jahns <kevin.jahns@protonmail.com>
2025-08-11 14:13:48 +0200

    more tests on transformers

--- delta/transformer.test.js
+++ delta/transformer.test.js
@@ -3,67 +3,68 @@ import * as dt from './transformer.js'
 import * as dmap from './map.js'
 import * as s from '../schema.js'
 
+const idt = dt.transformer({
+  $in: dmap.$deltaMap(s.$object({ x: s.$string })),
+  $out: dmap.$deltaMap(s.$object({ x: s.$string })),
+  state: () => null,
+  applyA: (d,state,def) => {
+    return dt.transformResult(null, d)
+  },
+  applyB: (d, state, def) => {
+    return dt.transformResult(d, null)
+  }
+})
+const mapString = dt.transformer({
+  $in: dmap.$deltaMap(s.$object({ x: s.$number })),
+  $out: dmap.$deltaMap(s.$object({ x: s.$string })),
+  state: () => null,
+  applyA: (d,state,def) => {
+    const dout = dmap.create(s.$object({ x: s.$string }))
+    d.forEach(op => {
+      if (dmap.$insertOpAny.check(op)) {
+        dout.set(op.key, op.value + '')
+      }
+    })
+    return dt.transformResult(null, dout)
+  },
+  applyB: (d, state, def) => {
+    const dout = dmap.create(s.$object({ x: s.$number }))
+    d.forEach(op => {
+      if (dmap.$insertOpAny.check(op)) {
+        dout.set(op.key, Number.parseInt(op.value))
+      }
+    })
+    return dt.transformResult(dout, null)
+  },
+})
+const mapNumber = dt.transformer({
+  $in: dmap.$deltaMap(s.$object({ x: s.$string })),
+  $out: dmap.$deltaMap(s.$object({ x: s.$number })),
+  state: () => null,
+  applyA: (d,state,def) => {
+    const dout = dmap.create(s.$object({ x: s.$number }))
+    d.forEach(op => {
+      if (dmap.$insertOpAny.check(op)) {
+        dout.set(op.key, Number.parseInt(op.value))
+      }
+    })
+    return dt.transformResult(null, dout)
+  },
+  applyB: (d, state, def) => {
+    const dout = dmap.create(s.$object({ x: s.$string }))
+    d.forEach(op => {
+      if (dmap.$insertOpAny.check(op)) {
+        dout.set(op.key, op.value + '')
+      }
+    })
+    return dt.transformResult(dout, null)
+  },
+})
+
 /**
  * @param {t.TestCase} _tc
  */
 export const testBasics = _tc => {
-  const idt = dt.transformer({
-    $in: dmap.$deltaMap(s.$object({ x: s.$string })),
-    $out: dmap.$deltaMap(s.$object({ x: s.$string })),
-    state: () => null,
-    applyA: (d,state,def) => {
-      return dt.transformResult(null, d)
-    },
-    applyB: (d, state, def) => {
-      return dt.transformResult(d, null)
-    }
-  })
-  const mapString = dt.transformer({
-    $in: dmap.$deltaMap(s.$object({ x: s.$number })),
-    $out: dmap.$deltaMap(s.$object({ x: s.$string })),
-    state: () => null,
-    applyA: (d,state,def) => {
-      const dout = dmap.create(s.$object({ x: s.$string }))
-      d.forEach(op => {
-        if (dmap.$insertOpAny.check(op)) {
-          dout.set(op.key, op.value + '')
-        }
-      })
-      return dt.transformResult(null, dout)
-    },
-    applyB: (d, state, def) => {
-      const dout = dmap.create(s.$object({ x: s.$number }))
-      d.forEach(op => {
-        if (dmap.$insertOpAny.check(op)) {
-          dout.set(op.key, Number.parseInt(op.value))
-        }
-      })
-      return dt.transformResult(dout, null)
-    },
-  })
-  const mapNumber = dt.transformer({
-    $in: dmap.$deltaMap(s.$object({ x: s.$string })),
-    $out: dmap.$deltaMap(s.$object({ x: s.$number })),
-    state: () => null,
-    applyA: (d,state,def) => {
-      const dout = dmap.create(s.$object({ x: s.$number }))
-      d.forEach(op => {
-        if (dmap.$insertOpAny.check(op)) {
-          dout.set(op.key, Number.parseInt(op.value))
-        }
-      })
-      return dt.transformResult(null, dout)
-    },
-    applyB: (d, state, def) => {
-      const dout = dmap.create(s.$object({ x: s.$string }))
-      d.forEach(op => {
-        if (dmap.$insertOpAny.check(op)) {
-          dout.set(op.key, op.value + '')
-        }
-      })
-      return dt.transformResult(dout, null)
-    },
-  })
   idt.pipe(idt)
   const x = mapNumber.pipe(mapString).pipe(mapNumber).pipe(mapString)
   idt.pipe(idt)
@@ -79,4 +80,20 @@ export const testBasics = _tc => {
     const d1_ = q.applyA(d1).b
     t.compare(d1, d1_)
   }
+}
+
+/**
+ * @param {t.TestCase} _tc
+ */
+export const testMapBasics = _tc => {
+  const m1 = dt.map({
+    mynum: mapNumber
+  }).init()
+  const d = dmap.create(s.$object({ x: s.$string })).set('x', '42').done()
+  const res = m1.applyA(d)
+  t.assert(res.a == null)
+  const qq = dmap.create(s.$object({ x: s.$number })).set('x', 42).done()
+  const q = dmap.create(s.$object({ mynum: dmap.$deltaMap(s.$object({ x: s.$number })) }) ).set('mynum', qq).done()
+  debugger
+  t.compare(res.b, q) // @todo not fixed
 }
